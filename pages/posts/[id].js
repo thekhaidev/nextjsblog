2afd99f09a27e82ff3f325fb17f945548140689d
Khai Tran <kqtran917@yahoo.com>
2021-10-24 18:00:39 -0500

    Implemented dynamic rendering of posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,7 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -18,6 +18,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
---
+++ posts/does-sara-have-drip.md
@@ -0,0 +1,6 @@
+---
+title: 'Does Sara have drip?'
+date: '2020-10-24'
+---
+
+Gonna be a no from me dawg
