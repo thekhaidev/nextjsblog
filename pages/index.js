361709c64a96f31711a5f072d78dca2c1dbb51b9
Khai Tran <kqtran917@yahoo.com>
2021-10-24 13:44:51 -0500

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello. This is Khai's world
         </h1>
 
         <p className="description">
@@ -49,14 +49,7 @@ export default function Home() {
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+    KHAI KHAI KKHAI KHAI
       </footer>
 
       <style jsx>{`
